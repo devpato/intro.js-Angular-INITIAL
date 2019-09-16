e81df4d7fdf653cb50f8c52866d318c0ec10cc5c
Patricio Vargas <pevargas2730@gmail.com>
2019-09-16 02:55:03 -0500

    added void

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,35 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  introJS = introJs();
-  constructor() {
-    this.introJS.setOptions({
-      steps: [
-        {
-          element: '#step1',
-          intro: 'Welcome to your new app!',
-          position: 'bottom'
-        },
-        {
-          element: '#step2',
-          intro: "Ok, wasn't that fun?",
-          position: 'right'
-        },
-        {
-          element: '#step3',
-          intro: "let's keep going",
-          position: 'top'
-        },
-        {
-          element: '#step4',
-          intro: 'More features, more fun.',
-          position: 'right'
-        }
-      ]
-    });
-  }
+  constructor() {}
 
-  ngOnInit() {
-    this.introJS.start();
-  }
+  ngOnInit(): void {}
 }
